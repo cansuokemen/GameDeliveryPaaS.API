c9adcb8608194257fa0bf1256b14353027051dcf
Cansu Ökemen <cansuokemen@gmail.com>
2025-04-23 15:30:47 +0300

    Added swagger NuGet packages

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer(); // bu önemli!
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger(); // Swagger JSON dosyasýný üretir
+    app.UseSwaggerUI(); // Arayüzü sunar
 }
 
 app.UseHttpsRedirection();
@@ -21,3 +21,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
